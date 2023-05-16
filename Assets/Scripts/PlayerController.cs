af1c4ae318719a5998f834f5cce9e9b3a0ddb627
jjc-kuwabara <kuwabara.ryosuke@nsg.gr.jp>
2023-05-16 17:45:29 +0900

    weapon追加

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,14 +5,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float jumpPower = 20.0f;
+
+    float attackAnimationSecond = 0.5f;
+    float weaponSwingAngleSpeedPerSecond = 180.0f;
+
+    float attackAnimeTimer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        attackAnimeTimer = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
+    {
+        UpdateMove();
+        UpdateJump();
+        UpdateAttack();
+
+    }
+
+    void UpdateMove()
     {
         float inputX = 0;
         float inputZ = 0;
@@ -33,11 +46,25 @@ public class PlayerController : MonoBehaviour
             inputX = 1;
         }
         RefreshPlayerPos(inputX, inputZ);
+    }
 
+    void UpdateJump()
+    {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GetComponent<Rigidbody>().AddForce(transform.up * jumpPower, ForceMode.Impulse);
         }
+    }
+
+    void UpdateAttack()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            float animationRate = attackAnimeTimer / attackAnimationSecond;
+            float degreeAngle = animationRate * weaponSwingAngleSpeedPerSecond;
+        }
+
+        attackAnimeTimer += Time.deltaTime;
     }
 
     void RefreshPlayerPos(float inputX, float inputZ)
